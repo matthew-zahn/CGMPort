49fc67240800fb809dfc1e59ff684cedd6c3700a
matthew-zahn <43350414+matthew-zahn@users.noreply.github.com>
2019-11-20 13:40:52 -0500

    Executed original fortran code.

--- SourceCode/Fortran/EV.f90
+++ SourceCode/Fortran/EV.f90
@@ -11,20 +11,6 @@ REAL, allocatable :: ones(:,:), inc2(:,:), v1(:,:), aux(:,:)
 ALLOCATE(ones(nrow,ncol),inc2(nrow,ncol),v1(nrow,ncol),aux(nrow,ncol))
 ev_out = 0.0
 ones= 1.0
-!$$$$$$ print *, 'The first argument in EV.f90 is', cash
-!$$$$$$ print *, 'The second argument in EV.f90 is', nrow
-!$$$$$$ print *, 'The third argument in EV.f90 is', ncol
-!$$$$$$ print *, 'The fourth argument in EV.f90 is', v
-!$$$$$$ print *, 'The fifth argument in EV.f90 is', nco
-!$$$$$$ print *, 'The sixth argument in EV.f90 is', prob
-!$$$$$$ print *, 'The seventh argument in EV.f90 is', n
-!$$$$$$ print *, 'The eighth argument in EV.f90 is', fy
-!$$$$$$ print *, 'The ninth argument in EV.f90 is', eyp
-!$$$$$$ print *, 'The tenth argument in EV.f90 is',grid
-!$$$$$$ print *, 'The eleventh argument in EV.f90 is',secondd
-!$$$$$$ print *, 'The twelth argument in EV.f90 is',ret
-!$$$$$$ print *, 'The thirteenth argument in EV.f90 is',reg_coef
-!$$$$$$ print *, 'The fourteenth argument in EV.f90 is',ev_out
 do ind1=1,n
   do ind2=1,n
      inc=(fy(ind1,1)*(eyp(ind2,1)+reg_coef*ret))
